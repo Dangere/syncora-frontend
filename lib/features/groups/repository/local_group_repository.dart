6792c188049b53ae6e14a810bc69b0b181a163fc
unknown <maowyt@gmail.com>
2025-04-23 23:53:18 +0200

    Implementing groups feature bare structure

---
+++ lib/features/groups/repository/local_group_repository.dart
@@ -0,0 +1,3 @@
+import 'package:syncora_frontend/features/groups/interfaces/group_repository_mixin.dart';
+
+class LocalGroupRepository implements GroupRepositoryMixin {}
---
+++ lib/features/groups/services/group_service.dart
@@ -0,0 +1,7 @@
+import 'package:syncora_frontend/features/groups/repository/local_group_repository.dart';
+
+class GroupService {
+  final LocalGroupRepository _groupRepository;
+
+  GroupService(this._groupRepository);
+}
